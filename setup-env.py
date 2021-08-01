9580ac9179d5d1ba865a15635dce56c12d9f760e
Tag-Epic <tagepicuwu@gmail.com>
2021-08-01 17:02:55 +0200

    Feat: Move away from git submodules to custom solution

---
+++ setup-env.py
@@ -0,0 +1,23 @@
+from sys import argv
+from shutil import rmtree
+from os import system
+
+git_mode = False
+if "git" in argv:
+    print("Cloning in git mode...")
+
+projects = open("projects").readlines()
+print(projects)
+
+for project in projects:
+    project = project.strip("\n")
+    try:
+        rmtree(project)
+    except FileNotFoundError:
+        print("Old files not found... ignoring")
+    if git_mode:
+        project_uri = "git@github.com:bluemediaapp/%s.git" % project
+    else:
+        project_uri = "https://github.com/bluemediaapp/%s" % project
+    system("git clone %s" % project_uri)
+
